26e684f03d18a11587e4a8c0af6927a58a4e74eb
Nikolaos Kotarelas <nikoskot1@gmail.com>
2026-06-06 17:52:07 +0300

    Separated model evaluation

--- yoloEvaluate.py
+++ yoloEvaluate.py
@@ -6,27 +6,50 @@ I will evaluate this model.
 
 from ultralytics import YOLO
 from ultralytics.utils.benchmarks import benchmark
+import argparse
+from prettytable import PrettyTable
+import numpy as np
+
+
+def getParser():
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--modelPath', required=True, help="The .pt model file to evaluate.")
+    parser.add_argument("--dataFilePath", type=str, default="data.yaml", help="The .yaml file that describes the dataset.")
+    parser.add_argument("--split", type=str, default="val", choices=["val", "train", "test"], help="The split to evaluate on.")
+    parser.add_argument("--imageSize", type=int, default=640, help="The image size that the model uses internally.")
+    parser.add_argument("--batchSize", type=int, default=4, help="The batch size for evaluation.")
+    
+    return parser
 
 
 def main():
-    model = YOLO("best.pt")
+
+    parser = getParser()
+    args = parser.parse_args()
+    
+    model = YOLO(args.modelPath)
 
     # Validate the model
-    # metrics = model.val(data="data.yaml", imgsz=640, batch=4, device=0, save_json=True)
-    # metrics.box.map  # map50-95
-    # metrics.box.map50  # map50
-    # metrics.box.map75  # map75
-    # metrics.box.maps  # a list containing mAP50-95 for each category
-    # metrics.box.image_metrics  # per-image metrics dictionary with precision, recall, F1, TP, FP, and FN
-
-    # Benchmark on GPU
-    # benchmark(model="best.pt", data="data.yaml", imgsz=640, half=False, int8=False, device=0, verbose=True, format="-")
-    # benchmark(model="best.pt", data="data.yaml", imgsz=640, half=False, int8=False, device=0, verbose=True, format="torchscript")
-    # benchmark(model="best.pt", data="data.yaml", imgsz=640, half=False, int8=False, device=0, verbose=True, format="onnx")
-    # benchmark(model="best.pt", data="data.yaml", imgsz=640, half=False, int8=False, device=0, verbose=True, format="engine")
+    metrics = model.val(data=args.dataFilePath, imgsz=args.imageSize, batch=args.batchSize, device=0, save_json=False, split=args.split)
     
-    # Export model to TensorRT engine
-    # model.export(format="engine", imgsz=640, half=True, device=0)
+    # Print metrics
+    allAPTable = PrettyTable(["Class", "AP50", "AP55", "AP60", "AP65", "AP70", "AP75", "AP80", "AP85", "AP90", "AP95"])
+    for i, cls in metrics.names.items():
+        allAPTable.add_row([cls] + np.round(metrics.box.all_ap[i], 5).tolist())
+    allAPTable.title = "All AP"
+    print(allAPTable)
+    
+    ap5095Table = PrettyTable(["Class", "AP50-95"])
+    for i, cls in metrics.names.items():
+        ap5095Table.add_row([cls, np.round(metrics.box.ap[i], 5)])
+    ap5095Table.title = "AP50-95 per Class"
+    print(ap5095Table)
+    
+    print(f"mAP50: {metrics.box.map50}\n")
+    print(f"mAP75: {metrics.box.map75}\n")
+    print(f"mAP50-95: {metrics.box.map}\n")
+
 
 if __name__ == "__main__":
     main()
